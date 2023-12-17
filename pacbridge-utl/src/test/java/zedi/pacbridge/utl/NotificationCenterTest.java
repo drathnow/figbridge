9dcc90815f746f920e5d5a37edc077d69968f409
Dave Rathnow <drathnow@gmail.com>
2023-12-17 08:20:30 -0700

    Upgraded versions of junit and mockito

--- pacbridge-utl/src/test/java/zedi/pacbridge/utl/NotificationCenterTest.java
+++ pacbridge-utl/src/test/java/zedi/pacbridge/utl/NotificationCenterTest.java
@@ -3,7 +3,7 @@ package zedi.pacbridge.utl;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertSame;
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
